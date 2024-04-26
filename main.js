23cd20388e593abc7f974d1b9c6f20d6531a49bf
Julio Aguilera Jr <julioaguilera337@gmail.com>
2024-04-25 19:25:14 -0500

    Fixed a typo

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ if (age < 18) {
     console.log("You are too young for the military!");
 } else if(nameString === "Rugen" && rank === "Count") {     //Name: "Rugen", Rank: "Count"
     console.log("My name is Inigo Montoya, you killed my father, prepare to die.");
-} else if (rank === "Captian") {    //has to be equal to Captian
+} else if (rank === "Captain") {    //has to be equal to Captain
     console.log(`Aye Aye ${rank}`);
 } else if (rank === "Private" && age > 40) {    //has to be equal to "Private" and be older than 40.
     console.log(`What have you been doing with your life ${nameString}?`);
